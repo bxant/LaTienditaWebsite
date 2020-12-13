8d276e7493bf12a5fe7ac677ed358951f6b34a3a
Ryuga64 <rvcervan@uci.edu>
2020-12-12 21:23:10 -0800

    added checkboxes to statspage

--- FinalProject/src/app/statistics/statistics.component.ts
+++ FinalProject/src/app/statistics/statistics.component.ts
@@ -4,17 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import * as productData from 'updatedProducts.json';
 
 
-class productType {
-  public id:String;
-  public label:String;
-  public isChecked:Boolean;
 
-  constructor(id:String,label:String,isChecked:Boolean) {
-    this.id = id;
-    this.label = label;
-    this.isChecked = isChecked;
-  }
-}
 
 @Component({
   selector: 'app-statistics',
@@ -25,84 +15,29 @@ export class StatisticsComponent implements OnInit {
 
   products: any = (productData as any).default;
 
-  public techProducts: Array<any>;
-  public groceryProducts: Array<any>;
-  public clothingProducts: Array<any>;
-  public toyProducts: Array<any>;
-  public hygieneProducts: Array<any>;
-  public kitchenProducts: Array<any>;
-  public dictTypes;
-  selectedType:Array<any>;
+  clothing:boolean = false;
+  electronics:boolean =false;  
+  groceries:boolean = false;
+  hygiene:boolean =false;  
+  kitchen:boolean = false;
+  toys:boolean =false;
 
-  selectedItemsList:productType[];
-  checkedIDs:String[];
-  // checkedIDs:productType[];
-  // other list
-  // checkedIDs:Array<any>;
-
-  checkboxDataList = [new productType('electronics', "Electronics", false),
-  new productType("groceries","Groceries",false),
-  new productType("clothing","Clothing",false),
-  new productType("toys","Toys",false),
-  new productType("hygiene","Hygiene",false),
-  new productType("kitchen","Kitchen",false)];
+  stuff:string[] = ['electronics','groceries','clothing','toys','hygiene','kitchen'];
   
   
-  constructor() { 
-    this.dictTypes = this.products[0];
-    this.techProducts = [];
-    this.groceryProducts = [];
-    this.clothingProducts = [];
-    this.toyProducts = [];
-    this.hygieneProducts = [];
-    this.kitchenProducts = [];
-    this.selectedType = []
-
-    this.selectedItemsList = [];
-    this.checkedIDs = [];
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.run();
-    this.fetchSelectedItems()
-    this.fetchCheckedIDs()
+    console.log(this.products[0]);
+    this.typeChosen();
   }
 
-  changeSelection() {
-    this.fetchSelectedItems()
+  typeChosen(){
+    console.log(this.clothing);
   }
 
-  fetchSelectedItems() {
-    this.selectedItemsList = this.checkboxDataList.filter((value, index) => {
-      return value.isChecked
-    });
-    console.log(this.selectedItemsList);
-  }
-
-  fetchCheckedIDs() {
-    this.checkedIDs = [];
-    this.checkboxDataList.forEach((value, index) => {
-      if (value.isChecked) {
-        this.checkedIDs.push(value.id);
-      }
-    });
-  }
-
-  run(){
-    this.techProducts = this.techProducts.concat(this.dictTypes['electronics']);
-    this.groceryProducts = this.groceryProducts.concat(this.dictTypes['groceries']);
-    this.clothingProducts = this.clothingProducts.concat(this.dictTypes['clothing']);
-    this.toyProducts = this.toyProducts.concat(this.dictTypes['toys']);
-    this.hygieneProducts = this.hygieneProducts.concat(this.dictTypes['hygiene']);
-    this.kitchenProducts = this.kitchenProducts.concat(this.dictTypes['kitchen']);
-  }
-
-
-
-  
-
-  
-
+  createList(){}
 
 
 }

7511cbaf48decec3d2c6237d38e5e61c9e372c2e
bxant <bryantflores54327@gmail.com>
2020-12-12 16:48:07 -0800

    made json file readable
    working on getting info

--- FinalProject/src/app/statistics/statistics.component.ts
+++ FinalProject/src/app/statistics/statistics.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+// jsonProducts file
+import * as productData from 'updatedProducts.json';
+
+
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent implements OnInit {
 
+  products: any = (productData as any).default;
+  public randomItem;
+  
   constructor() { }
 
   ngOnInit(): void {
+    console.log(productData);
   }
+
+  getTechProducts()
+  {
+    
+  }
+
+  getRandomItem()
+  {
+    this.randomItem = productData[0];
+  }
+
+
 
 }

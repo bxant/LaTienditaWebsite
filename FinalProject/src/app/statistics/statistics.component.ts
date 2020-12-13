0121e9d2422a60a9aae53bd30cdfdde0940ab537
pabloeram <pablorsencion510@gmail.com>
2020-12-12 17:37:45 -0800

    new update 5:37
    5:37 update

--- FinalProject/src/app/statistics/statistics.component.ts
+++ FinalProject/src/app/statistics/statistics.component.ts
@@ -13,23 +13,42 @@ export class StatisticsComponent implements OnInit {
 
   products: any = (productData as any).default;
 
-  techProducts:Array<any>;
+  public techProducts: Array<any>;
+  public groceryProducts: Array<any>;
+  public clothingProducts: Array<any>;
+  public toyProducts: Array<any>;
+  public hygieneProducts: Array<any>;
+  public kitchenProducts: Array<any>;
+  public dictTypes;
   
   constructor() { 
+    this.dictTypes = this.products[0];
     this.techProducts = [];
+    this.groceryProducts = [];
+    this.clothingProducts = [];
+    this.toyProducts = [];
+    this.hygieneProducts = [];
+    this.kitchenProducts = [];
   }
 
   ngOnInit(): void {
+    this.run()
     console.log("im productData")
-    console.log(productData);
+    console.log(this.dictTypes);
     console.log("im products")
-    console.log(this.products[0]);
+    console.log(this.techProducts);
   }
 
-  getTechProducts()
-  {
-    
+  run(){
+    this.techProducts = this.techProducts.concat(this.dictTypes['electronics']);
+    this.groceryProducts = this.groceryProducts.concat(this.dictTypes['groceries']);
+    this.clothingProducts = this.clothingProducts.concat(this.dictTypes['clothing']);
+    this.toyProducts = this.toyProducts.concat(this.dictTypes['toys']);
+    this.hygieneProducts = this.hygieneProducts.concat(this.dictTypes['hygiene']);
+    this.kitchenProducts = this.kitchenProducts.concat(this.dictTypes['kitchen']);
   }
+
+  
 
   
 

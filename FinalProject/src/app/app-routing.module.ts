32c3f8acc46b20ce3b4ab31e050cf4a01325d088
Ryuga64 <rvcervan@uci.edu>
2020-12-12 16:12:59 -0800

    configured routing

--- FinalProject/src/app/app-routing.module.ts
+++ FinalProject/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CATSComponent } from './cats/cats.component' 
+import { NearMeComponent } from './near-me/near-me.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'Cats', component: CATSComponent},
+{ path: 'Near', component: NearMeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
